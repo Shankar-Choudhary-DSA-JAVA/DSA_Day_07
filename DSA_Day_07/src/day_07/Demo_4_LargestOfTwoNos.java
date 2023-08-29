2fb0f4004fa922a4fdac258a87bac73ed02e79b3
Shankar <shankarallen2003@gmail.com>
2023-08-29 17:51:54 +0530

    DSA_Day_07 Commit

---
+++ DSA_Day_07/src/day_07/Demo_4_LargestOfTwoNos.java
@@ -0,0 +1,18 @@
+package day_07;
+
+public class Demo_4_LargestOfTwoNos {
+
+	public static void main(String[] args) {
+
+		int a=5;
+		int b=8;
+		
+		if (a>b) {
+			System.out.println("true");;
+		}else {
+			System.out.println("false");;
+
+		}
+	}
+
+}
